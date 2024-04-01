e1255292e997ea51b83ce6eaf426092cd1106efc
jialin <wao1204080218@gmail.com>
2024-04-01 23:15:03 +0800

    final version

--- import-test.py
+++ import-test.py
@@ -1,6 +1,10 @@
 import random
+start = input('請決定隨機數字範圍開始值：')
+end = input('請決定隨機數字範圍結束值：')
+start = int(start)
+end = int(end)
 
-r = random.randint(1, 100)
+r = random.randint(start, end)
 count = 0
 
 while True:
@@ -17,7 +21,7 @@ while True:
 		print('比答案小')
 	print('這是你猜的第', count, '次')
 
-	
+
 
 	#else:
 		#if num < r:
